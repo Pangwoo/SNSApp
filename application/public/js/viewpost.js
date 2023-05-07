2a24ba85bea8719def1d369fad042abc852838a7
Pangwoo <gwangwoo25@gmail.com>
2023-05-07 03:00:02 -0700

    For M2

---
+++ application/public/js/viewpost.js
@@ -0,0 +1,9 @@
+// const videoObserver = new IntersectionObserver(function (entries){
+//     if (entries[0].isIntersecting){
+//         document.getElementById("jump-to-top-button").style.disply = "none";
+//     }else{
+//         document.getElementById("jump-to-top-button").style.disply = "inline-block";        
+//     }
+// })
+
+// videoObserver.observe(document.querySelector(".post-container > video"));
--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Gwangwoo Lee" });
+  res.render('index', { title: 'CSC 317 App', name:"Gwangwoo Lee", js:["fetchMainContents.js"] });
 });
+
+router.get('/login', function(req, res, next){
+  res.render('login', { title: 'Log in' });
+});
+
+router.get('/registration', function(req, res){
+  res.render('registration', { title: 'Registration', js:["validation.js"] });
+});
+
+router.get('/profile', function(req, res){
+  res.render('profile', { title: 'Profile' });
+});
+
+
+
+// router.use("/postVideo", function(req,res,next){
+//   if(req.userIsLoggedIn){
+//     next();
+//   }else{
+//     res.redirect('/users/login');
+//   }
+// });
+
+router.get('/postVideo', function(req, res){
+  res.render('postVideo', { title: 'Post Video' });
+});
+
+router.get('/viewpost/:id(\\d+)', function(req, res){
+  res.render('viewpost', { title: `View Post ${req.params.id}`, js:["viewpost.js"] });
+});
+
 
 module.exports = router;
