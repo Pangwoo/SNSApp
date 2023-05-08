e6096384c2b9f045512ec549a645d847ea5d6cc0
Pangwoo <gwangwoo25@gmail.com>
2023-05-08 02:01:17 -0700

    update for milestone3

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+var db = require('../conf/database');
+/* GET localhost:3000/users */
+router.get('/', async function(req, res, next) {
+    try{
+      let [rows, fields] = await db.query(`select * from users;`);
+      res.status(200).json({json, fields});
+    }catch(error){
+      next(error);
+    }
+});
 
 // router.get('/login', function(req, res){
 //   res.render('login');
