a303c0a9f5423a602d9d65b464d4ad51a3ed9812
Pangwoo <gwangwoo25@gmail.com>
2023-05-08 18:08:24 -0700

    update post registration

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,18 +1,62 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../conf/database');
+
+
 /* GET localhost:3000/users */
-router.get('/', async function(req, res, next) {
-    try{
-      let [rows, fields] = await db.query(`select * from users;`);
-      res.status(200).json({json, fields});
-    }catch(error){
-      next(error);
-    }
-});
+// router.get('/', async function(req, res, next) {
+//     try{
+//       let [rows, fields] = await db.query(`select * from users;`);
+//       res.status(200).json({json, fields});
+//     }catch(error){
+//       next(error);
+//     }
+// });
+
+// router.use("/registration", function(req, res, next){
+  
+// })
 
 // router.get('/login', function(req, res){
 //   res.render('login');
 // })
+
+//localhost:3000/users/registration/
+router.post('/registration', async function(req,res,next){
+  var {username, email, password} = req.body;
+  try{
+    var [rows, fields] = await db.execute(`select id from users where username=?;`,[username]);
+    if(rows && rows.length > 0){
+      return res.redirect('/registration');
+    }
+    var [rows, fields] = await db.execute(`select id from users where email=?;`,[email]);
+    if(rows && rows.length > 0){
+      return res.redirect('/registration');
+    }
+    var [resultObject, fields] = await db.execute(`INSERT INTO users(username, email, password) value(?,?,?);`,[username, email, password]);
+    if(resultObject && resultObject.affectedRows == 1){
+      return res.redirect("/login");
+    }else{
+      return res.redirect("/registration");
+    }
+  }catch(error){
+    next(error);
+  }
+});
+
+// router.post('/login', async function(req,res,next){
+//   var {username, password} = req.body;
+//   try{
+//     var [rows, fields] = await db.execute(`SELECT id FROM  csc317db.users where username=? AND password=?;`, [username, password]);
+//     if(rows && rows.length > 0){
+//       console.log(rows);
+//     }
+    
+//     res.end();
+//   }catch(error){
+//     next(error);
+//   }
+// })
+
 
 module.exports = router;
